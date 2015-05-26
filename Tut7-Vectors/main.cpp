5f205a27db046549f61503eaf53eea68834a9231
mrmakhura <Stonewall305>
2015-05-26 16:32:05 +0200

    2nd intial commit

--- Tut7-Vectors/main.cpp
+++ Tut7-Vectors/main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <algorithm>  // for auto sort
+#include <vector>	  // for auto stack
 
 using namespace std;
 
 int main()
 {
+	int x;
+
+	vector<int> arrInt;
+	for (int i = 0; i < 20; ++i)
+	{
+		cout << "Please enter integer : ";
+		cin >> x;
+		arrInt.push_back(x);
+	}
+
+	sort(arrInt.begin(), arrInt.end());
+
+
 
 }
